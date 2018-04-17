e89cea7837443e9e91a8708d8851f4840184b618
Milly <millydack@gmail.com>
2018-04-17 09:36:58 +0200

    3.2 con level1

--- practica3/practica3/practica3.js
+++ practica3/practica3/practica3.js
@@ -1,9 +1,18 @@
 var game = function(){
 
 	var Q = window.Q =  Quintus()
-            .include("Scenes, Sprites, Input, UI, Touch")
+            .include("Scenes, Sprites, Input, UI, Touch, TMX, Anim, 2D")
             .setup({
                 width: 320,
                 height: 480,
     		}).controls().touch();
+
+    Q.scene("level1", function(stage){
+    	Q.stageTMX("level.tmx", stage);
+    });
+
+    Q.loadTMX("level.TMX", function(){
+    	Q.stageScene("level1");
+    })
+
 };
