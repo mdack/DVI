3a3f9a210695582eae4f998421746bee54e48ba4
Milly <millydack@gmail.com>
2018-04-18 14:51:10 +0200

    3.3

--- practica3/practica3/practica3.js
+++ practica3/practica3/practica3.js
@@ -9,10 +9,48 @@ var game = function(){
 
     Q.scene("level1", function(stage){
     	Q.stageTMX("level.tmx", stage);
+    })
+
+    Q.loadTMX("level.tmx", function(){
+        //Q.stageScene("mainTitle.png")
+    	Q.stageScene("level1");
+    })
+
+    Q.Sprite.extend("Mario", {
+        init: function(p) {
+            this._super(p, {
+                sprite: "mario_anim",
+                sheet: "mario",
+                x: 150,
+                y: 380
+            });
+
+            this.add("2d, platformerControls");
+        },
+        step: function(dt){
+            if(this.p.vx > 0){
+                this.play("marioR");
+            }else if(this.p.vx < 0){
+                this.play("marioL");
+            }else{
+                this.play("stand_" + this.p.direction);
+            }
+        }
     });
 
-    Q.loadTMX("level.TMX", function(){
-    	Q.stageScene("level1");
+    Q.animations("mario_anim", {
+        "marioR":{frames: [1,2,3], rate: 1/10},
+        "marioL":{frames: [15,16,17], rate: 1/10},
+        "stand_right":{frames: [0], rate: 1/10, loop: false},
+        "stand_left":{frames: [14], rate: 1/10, loop: false},
+        "jumping_right":{frames: [4], rate: 1/10, loop: false},
+        "jumping_left":{frames: [18], rate: 1/10, loop: false},
+        "mario_die":{frames: [12], rate: 1/10, loop: false}
+    });
+
+    Q.load(["mario_small.png", "mario_small.json"], function(){
+        //this will create the sprite sheets
+        Q.compileSheets("mario_small.png", "mario_small.json");
     })
 
 };
