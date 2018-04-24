4594092c5fe344659b2194a6179a88c014f022f1
Milly <millydack@gmail.com>
2018-04-24 11:56:38 +0200

    Añadidos más puntos

--- practica3/practica3.js
+++ practica3/practica3.js
@@ -39,14 +39,129 @@ var game = function(){
         "mario_die":{frames: [12], rate: 1/10, loop: false}
     });
 
+    Q.Sprite.extend("Goomba", {
+        init: function(p){
+            this._super(p, {
+                sprite: "goomba_anim",
+                sheet: "goomba",
+                x: 1200,
+                y: 380,
+                vx: 100
+            });
+
+            this.add("2d, aiBounce");
+
+            this.on("bump.left,bump.right",function(collision) {
+                if(collision.obj.isA("Mario")) {
+                    //Q.stageScene("endGame",1, { label: "You Died" });
+                    collision.obj.destroy();
+                }
+            });
+
+            this.on("bump.top", function(collision){
+                if(collision.obj.isA("Mario")){
+                    this.entity.play("goomba_die");
+                    collision.obj.p.vy = -300;
+                }
+            });
+        },
+        step: function(dt){
+
+        }
+    });
+
+    Q.animations("goomba_anim", {
+        "goomba_walk": {frames: [1], rate: 1/5},
+        "goomba_die": {frames: [2,3], rate: 1/3, loop:false}
+    });
+
+    Q.Sprite.extend("Bloopa", {
+        init: function(p){
+            this._super(p, {
+                sprite: "bloopa_anim",
+                sheet: "bloopa",
+                frame: 0,
+                gravity: 0,
+                x: 500,
+                y: 380,
+                vy: 100,
+                collisioned: false
+            });
+
+            this.add("2d, aiBounce");
+
+            this.on("bump.left,bump.right, bump.bottom",function(collision) {
+                if(collision.obj.isA("Mario")) {
+                    //Q.stageScene("endGame",1, { label: "You Died" });
+                    collision.obj.destroy();
+                }
+            });
+
+            this.on("bump.top", function(collision){
+                if(collision.obj.isA("Mario")){
+                    this.entity.play("bloopa_die");
+                    collision.obj.p.vy = -300;
+                }
+            });
+        },
+        step: function(dt){
+                this.timeJump += dt;
+
+                if(this.p.vy == 0){
+                    this.p.vy = -50;
+                    this.timeJump = 0;
+                }
+
+                if (this.timeJump >= 2)
+                    this.p.vy = 120;
+                }
+        }
+    });
+
+    Q.animations("bloopa_anim", {
+        "bloopa_walk": {frames: [0,1], rate: 1/3},
+        "bloopa_die": {frames: [2], rate: 1/3, loop:false}
+    });
+
+    Q.scene("endGame", function(stage) {
+        var container = stage.insert(new Q.UI.Container({
+            x: Q.width/2, y: Q.height/2, fill: "rgba(0,0,0,0.5)"
+        }));
+        var button = container.insert(new Q.UI.Button({ x: 0, y: 0, fill: "#CCCCCC", label: "Play Again" }));
+        var label = container.insert(new Q.UI.Text({x:10, y: -10 - button.p.h, label: stage.options.label }));
+        // When the button is clicked, clear all the stages
+        // and restart the game.
+        button.on("click",function() {
+        Q.clearStages();
+        Q.stageScene('level1');
+        });
+
+        container.fit(20);
+    });
+
+    Q.scene("mainTitle", function(stage){
+        var container = stage.insert(new Q.UI.Container({x: Q.width, y: Q.height}));
+        var button = container.insert(new Q.UI.Button({x: -Q.width/2, y: -Q.height/2, fill: "#CCCCCC", asset: "mainTitle.png"}));
+        var label = container.insert(new Q.UI.Text({x: 0, y: 10, label: "Press Enter or click to start", size: 18, color: "black"}));
+        
+        button.on("click", function(){
+            Q.clearStages();
+            Q.stageScene("level1");
+        });
+
+        container.fit(20);
+    });
+
     Q.scene("level1", function(stage){
         Q.stageTMX("level.tmx", stage);
 
         var player = stage.insert(new Q.Mario());
         stage.add("viewport").follow(player);
+        stage.viewport.offsetX = -100;
+        stage.viewport.offsetY = 150;
     })
 
-    Q.load("level.json, mario_small.png, mario_small.json", function(){
+    Q.load("mario_small.png, mario_small.json", function(){
         //this will create the sprite sheets
         Q.compileSheets("mario_small.png", "mario_small.json");
         Q.stageScene("level1");
